6ec760a1bed6dcd2a2e8c11c1173e89dcd8135f7
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-29 17:53:59 +0200

    Main_menu
    Main menu con los Toast

--- app/src/main/java/com/example/isaac/plantillam8uf1/MainActivity.java
+++ app/src/main/java/com/example/isaac/plantillam8uf1/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.isaac.plantillam8uf1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,28 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuPlatosID:
+                Toast.makeText(this, "Ver platos menu", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuHacerReservaID:
+                Toast.makeText(this, "Hacer reserva", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuVerReservaID:
+                Toast.makeText(this, "Ver reserva", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
