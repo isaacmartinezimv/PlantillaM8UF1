f2a60c8a0a991f4ffad69e4496836993b2deee74
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-29 22:30:51 +0200

    Fragments working
    logic of fragments added

--- app/src/main/java/com/example/isaac/plantillam8uf1/MainActivity.java
+++ app/src/main/java/com/example/isaac/plantillam8uf1/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.isaac.plantillam8uf1;
 
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,18 +11,40 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements
+FragmentPlatos.OnFragmentInteractionListener,
+FragmentHacerReserva.OnFragmentInteractionListener,
+FragmentVerReservas.OnFragmentInteractionListener,
+FragmentWellcome.OnFragmentInteractionListener{
 
     Toolbar toolbar;
+
+    //Delcaramos variables tipo Fragment
+    FragmentWellcome wellcomeScreen;
+    FragmentPlatos platosMenu;
+    FragmentHacerReserva hacerReserva;
+    FragmentVerReservas verReservas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Deshabilitamos la rotacion de la pantalla
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //Asociamos las variables a un nuevo fragment
+        wellcomeScreen = new FragmentWellcome();
+        platosMenu = new FragmentPlatos();
+        hacerReserva = new FragmentHacerReserva();
+        verReservas = new FragmentVerReservas();
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Para mostrar 1 frament nada mas empezar, hacemos esto, NO OLVIDAR COMMIT()
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments,wellcomeScreen).commit();
     }
 
     @Override
@@ -31,17 +56,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        //Declaramos una varible tipo FragmentTransaction
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
         switch (item.getItemId()){
             case R.id.menuPlatosID:
-                Toast.makeText(this, "Ver platos menu", Toast.LENGTH_SHORT).show();
+                transaction.replace(R.id.contenedorFragments, platosMenu);
                 break;
             case R.id.menuHacerReservaID:
-                Toast.makeText(this, "Hacer reserva", Toast.LENGTH_SHORT).show();
+                transaction.replace(R.id.contenedorFragments, hacerReserva);
                 break;
             case R.id.menuVerReservaID:
-                Toast.makeText(this, "Ver reserva", Toast.LENGTH_SHORT).show();
+                transaction.replace(R.id.contenedorFragments, verReservas);
                 break;
         }
+
+        transaction.commit();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
